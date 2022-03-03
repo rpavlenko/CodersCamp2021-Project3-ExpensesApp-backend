5192a6cb5e94c14f3645638823f2684f58bd3056
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-03-03 21:33:00 +0100

    add register endpoint

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -10,6 +10,24 @@ const loginUser = async (req, res) => {
   }else res.status(400).send({ code: 0 });
 };
 
+const registerUser = async (req, res) => {
+  const userExist = await Users.findOne({ email: req.body.email });
+  if(userExist) res.status(409).send({ code: 0 });
+  else {
+    const userData = {
+      ...req.body,
+      isActive: true,
+      createdAt: Date()
+    };
+    const user = new Users(userData);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    res.status(200).send({ code: 1 });
+  }
+};
+
 module.exports = {
   loginUser,
+  registerUser
 };
--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  passowrd: {
+  password: {
     type: String,
     required: true
   },
