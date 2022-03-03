554206be983c8631a8ad0437052e063182fab06a
magdalenaz-c <zaniewska.cmagdalena@gmail.com>
2022-03-03 21:30:52 +0100

    add endpoints for transactions

--- models/Transactions.js
+++ models/Transactions.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
-const TransactionsSchema = mongoose.Schema({});
+const TransactionsSchema = mongoose.Schema({
+  // userID: {
+  //   type: mongoose.SchemaTypes.ObjectId,
+  //   required: true,
+  // },
+  category: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('Transactions', TransactionsSchema);
