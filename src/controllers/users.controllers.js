d8430efae2b7eb6f339c2eafee4064cdc6f20c96
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-03-07 19:37:53 +0100

    block sign in when account inactive

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -5,7 +5,7 @@ const Users = require('../models/User');
 
 const loginUser = async (req, res) => {
   const userExist = await Users.findOne({ email: req.body.email });
-  if(userExist){
+  if(userExist && userExist.isActive){
     const passwordValidation = await bcrypt.compare(req.body.password, userExist.password);
     if(passwordValidation) res.status(200).send({ code: 1 });
     else res.status(400).send({ code: 0 });
