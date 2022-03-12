ad10596d6e02b1b1a3adf4e3dd8405b6934ddd8d
Barbara <bkorytkowska@gmail.com>
2022-03-12 17:53:09 +0100

    add userId

--- src/controllers/balance.controllers.js
+++ src/controllers/balance.controllers.js
@@ -3,7 +3,7 @@ const Transactions = require('../models/Transactions');
 const getBalance = async (req, res) => {
   const userId = req.userId;
   try {
-    const transactions = await Transactions.find(); // w dokumentacji mongoose znajdź metodę modelu która zwróci wszystke elementy kolekcji Transactions o parametrze 'userID' = userId
+    const transactions = await Transactions.find({ userID: userId});
     const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((acc, item) => (acc += item), 0);
     res.json(total);
