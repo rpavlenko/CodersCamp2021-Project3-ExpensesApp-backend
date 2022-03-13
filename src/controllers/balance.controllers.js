62050615224a11f92c78cac291e9d35c0109ba57
Barbara <bkorytkowska@gmail.com>
2022-03-13 12:14:04 +0100

    balance

--- src/controllers/balance.controllers.js
+++ src/controllers/balance.controllers.js
@@ -4,10 +4,10 @@ const getBalance = async (req, res) => {
   const userId = req.userId;
   try {
     const transactions = await Transactions.find({ userID: userId});
-    const amounts = transactions.map(transaction => transaction.amount);add
+    const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((acc, item) => (acc += item), 0);
     res.json(total);
-  } catch (err) {
+  } catch (err) { comm
     res.json({ message: err });
   }
 };
