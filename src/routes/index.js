920df86d2e7b90a86e582c68775f58a70777d970
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-03-23 22:12:29 +0100

    add api/v1 to url

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { authToken } = require('../middleware/auth');
 
-router.use('/balance', authToken, require('./balance.router'));
-router.use('/categories', authToken, require('./categories.router'));
-router.use('/transactions', authToken, require('./transactions.router'));
-router.use('/users', require('./users.router'));
+router.use('/api/v1/balance', authToken, require('./balance.router'));
+router.use('/api/v1/categories', authToken, require('./categories.router'));
+router.use('/api/v1/transactions', authToken, require('./transactions.router'));
+router.use('/api/v1/users', require('./users.router'));
 
 module.exports = router;
