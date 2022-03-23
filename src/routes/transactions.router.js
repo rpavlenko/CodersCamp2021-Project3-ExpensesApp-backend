805d22012a954b65d6c8d27f3945395bc95dcee9
Barbara <bkorytkowska@gmail.com>
2022-03-23 20:52:53 +0100

    routes

--- src/routes/transactions.router.js
+++ src/routes/transactions.router.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const {
   getTransactions, getTransaction, addTransactions,
   updateTransaction,
